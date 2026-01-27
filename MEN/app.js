785862caaaf4ee4ab0e8db1ed60ad11e1d5bbab5
shashidhar078 <nagunurishashidhar@gmail.com>
2026-01-27 20:18:50 +0530

    revised basics of node and explored server creation & basic routing in node in MEN folder

---
+++ MEN/app.js
@@ -0,0 +1,18 @@
+const http=require('http')
+
+
+//example for routing 
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if(req.url=="/about")
+    {
+        res.end("This is about page");
+    }
+    if(req.url=="/profile")
+    {
+        res.end("This is profile page")
+    }
+    res.end("Hello world");
+})
+
+server.listen(3000)
