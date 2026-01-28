ca5aecaf5470701e1228acbc8e031a773fc9616b
shashidhar078 <nagunurishashidhar@gmail.com>
2026-01-28 22:30:16 +0530

    understood middlewares and its types

--- MENEXPRESS/app.js
+++ MENEXPRESS/app.js
@@ -1,9 +1,29 @@
 const express=require('express')
+const morgan=require('morgan')
 const app=express();
 
 //implementing ejs 
 app.set("view engine","ejs")
-app.get("/about",(req,res)=>{
+
+//using third party middleware morgan
+app.use(morgan('dev'))
+//it is used for logging
+//explains which route is accessed and how much reponse time it took
+
+//application-level-middleware
+app.use((req,res,next)=>{
+    console.log("This is a  middleware")
+    a=20
+    b=10
+    console.log(a+b)
+    next();
+})
+app.get("/about",(req,res,next)=>{
+console.log("This is from about")
+const a="shashi"
+console.log(`this is ${a}`)
+next();
+},(req,res)=>{
     res.send("This is About page");
     
 })
